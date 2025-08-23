731168eafcae5f87dccd519d1ea8f75bbd209743
RadoslavGeorgiev71 <R.K.Georgiev@student.tudelft.nl>
2025-08-23 17:56:22 +0300

    Data changes.

--- src/types/state.ts
+++ src/types/state.ts
@@ -1,14 +1,15 @@
 export enum State {
-  preTask = "preTask",
-
   instructions = "instructions",
-  tutorial = "tutorial",
-  task1 = "task1",
-  postTask1 = "postTask1",
-  task2 = "task2",
-  postTask2 = "postTask2",
-  task3 = "task3",
-  postTask3 = "postTask3",
+
+  preTask1 = "preTask1",
+  finance = "finance",
+
+  preTask2 = "preTask2",
+  reviews = "reviews",
+
+  preTask3 = "preTask3",
+  medical = "medical",
+
   postTask = "postTask",
   revokedConsent = "revokedConsent",
   completion = "completion",
